e39f42a54c2140685c1521f25e85f19b6da1a1c2
Marcin Polak <mapoart@gmail.com>
2020-09-02 18:54:02 +0200

    removed -y on brew install for MacOS

--- php.darwin.nexss.config.js
+++ php.darwin.nexss.config.js
@@ -1,7 +1,7 @@
 let languageConfig = Object.assign({}, require("./php.linux.nexss.config"));
 languageConfig.compilers = {
   php7: {
-    install: "brew install -y php",
+    install: "brew install php",
     command: "php",
     args: "<file>",
     help: ``,
