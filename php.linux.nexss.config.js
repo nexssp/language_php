66c233b7eff521355323c3882c2a4b3fe1227ca4
Marcin Polak <mapoart@gmail.com>
2020-09-02 19:46:13 +0200

    removed sudo from linux part.

--- php.linux.nexss.config.js
+++ php.linux.nexss.config.js
@@ -2,7 +2,7 @@ let languageConfig = Object.assign({}, require("./php.win32.nexss.config"));
 languageConfig.errors = require("./nexss.php.errors");
 languageConfig.compilers = {
   php7: {
-    install: "sudo apt install -y php",
+    install: "apt install -y php",
     command: "php",
     args: "<file>",
     help: ``,
@@ -11,7 +11,7 @@ languageConfig.compilers = {
 languageConfig.languagePackageManagers = {
   composer: {
     installation:
-      "sudo apt-get update && sudo apt-get install curl && sudo curl -s https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer",
+      "apt update && apt install curl && curl -s https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer",
     messageAfterInstallation:
       "Add to the top of your php file(s): require __DIR__ . '/vendor/autoload.php';", //this message will be displayed after this package manager installation, maybe some action needed etc.
     installed: "composer installed",
