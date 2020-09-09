c0d43772dc06f2099f656329c8bccb5c40405bac
Marcin Polak <mapoart@gmail.com>
2020-09-10 00:33:59 +0200

    Added Arch, CentOS, Fedora, Rhel options

--- php.linux.nexss.config.js
+++ php.linux.nexss.config.js
@@ -29,6 +29,10 @@ languageConfig.languagePackageManagers = {
 
 // If statement must be here for older versions nexss <2.1.12
 if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
+  languageConfig.languagePackageManagers.composer.installation = `php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '8a6138e2a05a8c28539c9f0fb361159823655d7ad2deecb371b04a83966c61223adc522b0189079e3e9e277cd72b8897') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"`;
   const dist = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`).dist;
   const distName = dist();
   switch (distName) {
@@ -40,9 +44,16 @@ if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
       break;
     case "Alpine Linux":
       languageConfig.compilers.php7.install = "apk add php php7-json";
-      languageConfig.languagePackageManagers.composer.installation =
-        "apk update && apk install curl && curl -s https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer";
       break;
+    case "Arch Linux":
+      languageConfig.compilers.php7.install = `pacman -S --noconfirm php php7-json`;
+      break;
+    case "FEDORA":
+      languageConfig.compilers.php7.install = `dnf install php php7-json`;
+      break;
+    case "CENTOS":
+    case "RHEL":
+      languageConfig.compilers.php7.install = "yum install php php7-json";
   }
 
   languageConfig.dist = distName;
