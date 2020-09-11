5126f0eee49eae7bee7bb9c0b97b225fc1465bf1
Marcin Polak <mapoart@gmail.com>
2020-09-11 11:38:09 +0200

    Added sudo and more for php

--- php.linux.nexss.config.js
+++ php.linux.nexss.config.js
@@ -38,10 +38,7 @@ if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
 php -r "if (hash_file('sha384', 'composer-setup.php') === '8a6138e2a05a8c28539c9f0fb361159823655d7ad2deecb371b04a83966c61223adc522b0189079e3e9e277cd72b8897') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"`;
-  const {
-    dist,
-    version,
-  } = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`).dist;
+  const { dist, version } = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
   const distName = dist();
 
   switch (distName) {
