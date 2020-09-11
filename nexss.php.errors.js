ed383267c7cd9a70ebf95283438216b0f357e98f
Marcin Polak <mapoart@gmail.com>
2020-09-11 14:32:17 +0200

    Added imap, json errors

--- nexss.php.errors.js
+++ nexss.php.errors.js
@@ -1,4 +1,8 @@
 module.exports = {
   "PHP Notice:  Undefined variable: (?<variable>.*?) in (?<folder>.*?) on line (?<line>.*?)$":
     "<variable> has not been found. Check <folder> on line <line>",
+  "Call to undefined function json_decode":
+    "You may need to run <your package manager> install php-json",
+  "Call to undefined function imap_open":
+    "You may need to run <your package manager> install php-imap",
 };
