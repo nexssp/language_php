be32001a2761adf353a53bce150262b770356e1b
Marcin Polak <mapoart@gmail.com>
2020-09-12 21:39:20 +0200

    Added better error (display with right install command depends on distro)

--- nexss.php.errors.js
+++ nexss.php.errors.js
@@ -1,8 +1,10 @@
+const {
+  getInstallCommandByDist,
+} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+
 module.exports = {
   "PHP Notice:  Undefined variable: (?<variable>.*?) in (?<folder>.*?) on line (?<line>.*?)$":
     "<variable> has not been found. Check <folder> on line <line>",
-  "Call to undefined function json_decode":
-    "You may need to run <your package manager> install php-json",
-  "Call to undefined function imap_open":
-    "You may need to run <your package manager> install php-imap",
+  "Call to undefined function json_decode": `You may need to run ${getInstallCommandByDist()} php-json`,
+  "Call to undefined function imap_open": `You may need to run ${getInstallCommandByDist()} php-imap`,
 };
