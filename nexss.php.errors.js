cafa4285644c5b89b8ad928e9c9de5f113e3e6cc
Marcin Polak <mapoart@gmail.com>
2020-10-20 18:18:30 +0200

    fixed error to 7.4

--- nexss.php.errors.js
+++ nexss.php.errors.js
@@ -7,6 +7,6 @@ module.exports = {
     "<variable> has not been found. Check <folder> on line <line>",
   "Call to undefined function json_decode": `You may need to run '${getInstallCommandByDist()} php-json'`,
   "Call to undefined function imap_open": `You may need to run '${getInstallCommandByDist()} php-imap' or
-enable the extension in the php.ini file by uncommenting or adding the line: extension = php_imap.dll.
+enable the extension in the php.ini file by uncommenting or adding the line: extension=imap.
 To check your php.ini location run 'php --ini'.`,
 };
