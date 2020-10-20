32ec0e25fc1f1a1c756dd8896634e28d33ec5e0a
Marcin Polak <mapoart@gmail.com>
2020-10-20 18:14:21 +0200

    Better errors - php_imap php7.4

--- nexss.php.errors.js
+++ nexss.php.errors.js
@@ -5,6 +5,8 @@ const {
 module.exports = {
   "PHP Notice:  Undefined variable: (?<variable>.*?) in (?<folder>.*?) on line (?<line>.*?)$":
     "<variable> has not been found. Check <folder> on line <line>",
-  "Call to undefined function json_decode": `You may need to run ${getInstallCommandByDist()} php-json`,
-  "Call to undefined function imap_open": `You may need to run ${getInstallCommandByDist()} php-imap`,
+  "Call to undefined function json_decode": `You may need to run '${getInstallCommandByDist()} php-json'`,
+  "Call to undefined function imap_open": `You may need to run '${getInstallCommandByDist()} php-imap' or
+enable the extension in the php.ini file by uncommenting or adding the line: extension = php_imap.dll.
+To check your php.ini location run 'php --ini'.`,
 };
