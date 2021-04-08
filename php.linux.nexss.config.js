d402a1d5329359a28af345b063c393f7deddd6c4
Marcin Polak <mapoart@gmail.com>
2021-04-08 13:12:13 +0200

    removed @nexsp/os dependency: /nexssp/cli/issues/36

--- php.linux.nexss.config.js
+++ php.linux.nexss.config.js
@@ -1,14 +1,17 @@
 let languageConfig = Object.assign({}, require("./php.win32.nexss.config"));
 languageConfig.errors = require("./nexss.php.errors");
 
-let sudo = "sudo ";
-if (process.getuid && process.getuid() === 0) {
-  sudo = "";
-}
+let sudo = process.sudo;
 
 // Ubuntu, Debian
 languageConfig.compilers = {
   php8: {
+    install: `${sudo}apt install -y php`,
+    command: "php",
+    args: "<file>",
+    help: ``,
+  },
+  php8ini: {
     install: `${sudo}apt install -y php`,
     command: "php",
     args:
@@ -33,50 +36,49 @@ languageConfig.languagePackageManagers = {
   },
 };
 
+let compilerConfig = {};
 // If statement must be here for older versions nexss <2.1.12
 if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
   languageConfig.languagePackageManagers.composer.installation = `php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '8a6138e2a05a8c28539c9f0fb361159823655d7ad2deecb371b04a83966c61223adc522b0189079e3e9e277cd72b8897') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"`;
-  const { dist, version } = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
-  const distName = dist();
+  const distName = process.distro;
   // TODO: Later to cleanup this config file !!
   switch (distName) {
     case "Oracle":
     case "Oracle Linux Server":
-      if (version) {
-        //if here for older versions of nexssp
-        const distVersion = version(); // *1 converts to number
-        if (distVersion >= 8) {
-          // TODO: recognize the slim version
-          languageConfig.compilers.php7.install = `${sudo}dnf update -y && ${sudo}dnf install -y oracle-epel-release* && ${sudo}dnf install -y php php-json`;
-          languageConfig.languagePackageManagers.composer.installation = `${sudo}dnf update -y && ${sudo}dnf install -y curl && curl -s https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer`;
-        } else {
-          languageConfig.compilers.php7.install = `${sudo}yum update -y && ${sudo}yum install -y oracle-epel-release* && ${sudo}yum install -y php php-json php-imap`;
-          languageConfig.languagePackageManagers.composer.installation = `${sudo}yum update -y && ${sudo}yum install -y curl && curl -s https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer`;
-        }
+      //if here for older versions of nexssp
+      const distVersion = process.distroVersion * 1; // *1 converts to number
+      if (distVersion >= 8) {
+        // TODO: recognize the slim version
+        compilerConfig.install = `${sudo}dnf update -y && ${sudo}dnf install -y oracle-epel-release* && ${sudo}dnf install -y php php-json`;
+        languageConfig.languagePackageManagers.composer.installation = `${sudo}dnf update -y && ${sudo}dnf install -y curl && curl -s https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer`;
+      } else {
+        compilerConfig.install = `${sudo}yum update -y && ${sudo}yum install -y oracle-epel-release* && ${sudo}yum install -y php php-json php-imap`;
+        languageConfig.languagePackageManagers.composer.installation = `${sudo}yum update -y && ${sudo}yum install -y curl && curl -s https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer`;
       }
+
       break;
     case "Alpine Linux":
-      languageConfig.compilers.php7.install = `${sudo}apk add php php7-json php-imap`;
+      compilerConfig.install = `${sudo}apk add php php7-json php-imap`;
       break;
     case "Arch Linux":
-      languageConfig.compilers.php7.install = `${sudo}pacman -Sy --noconfirm php php-imap`;
+      compilerConfig.install = `${sudo}pacman -Sy --noconfirm php php-imap`;
       break;
     case "Fedora":
-      languageConfig.compilers.php7.install = `${sudo}dnf install -y php php-json php-imap`;
+      compilerConfig.install = `${sudo}dnf install -y php php-json php-imap`;
       break;
     case "CentOS Linux":
-      languageConfig.compilers.php7.install = `${sudo}yum install -y epel-release* && yum install -y php php-json php-imap`;
+      compilerConfig.install = `${sudo}yum install -y epel-release* && yum install -y php php-json php-imap`;
       break;
     case "openSUSE Leap":
     case "openSUSE Tumbleweed":
-      languageConfig.compilers.php7.install = `${sudo}zypper -n install php7 php7-json`;
+      compilerConfig.install = `${sudo}zypper -n install php7 php7-json`;
       break;
     case "Amazon Linux":
     case "RHEL Linux":
-      languageConfig.compilers.php7.install = `${sudo}yum install -y php php-json php-imap`;
+      compilerConfig.install = `${sudo}yum install -y php php-json php-imap`;
       break;
     default:
   }
@@ -85,5 +87,14 @@ php -r "unlink('composer-setup.php');"`;
 } else {
   languageConfig.dist = "Ubuntu";
 }
+
+languageConfig.compilers.php8 = Object.assign(
+  languageConfig.compilers.php8,
+  compilerConfig
+);
+languageConfig.compilers.php8ini = Object.assign(
+  languageConfig.compilers.php8ini,
+  compilerConfig
+);
 
 module.exports = languageConfig;
