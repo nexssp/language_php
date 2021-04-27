0a529e0689c78c3191641b1a977d7b776da6484a
Marcin Polak <mapoart@gmail.com>
2021-04-27 21:20:13 +0200

    Added patch for php 5.3 for templates (AMAZON AMI - AWS notebooks)

--- templates/default.php
+++ templates/default.php
@@ -3,12 +3,16 @@
 # Default template for JSON Data
 # STDIN
 $NexssStdin = fgets(STDIN);
-$parsedJson = json_decode($NexssStdin,true);
+$parsedJson = json_decode($NexssStdin, true);
 
 # Modify Data
 $parsedJson["test"] = "test";
 // $parsedJson["test"] = "test";
-$NexssStdout = json_encode($parsedJson, JSON_UNESCAPED_UNICODE);
+if (version_compare(phpversion(), '5.4.0', '<')) {
+    $NexssStdout = json_encode($parsedJson);
+} else {
+    $NexssStdout = json_encode($parsedJson, JSON_UNESCAPED_UNICODE);
+}
 
 # STDOUT
 fwrite(STDOUT, $NexssStdout);
--- templates/helloWorld.php
+++ templates/helloWorld.php
@@ -3,12 +3,17 @@
 # Default template for JSON Data
 # STDIN
 $NexssStdin = fgets(STDIN);
-$parsedJson = json_decode($NexssStdin,true);
+$parsedJson = json_decode($NexssStdin, true);
 
 # Modify Data
-$parsedJson["phpOutput"] = "Hello from PHP! " . (string)phpversion() ;
+$parsedJson["phpOutput"] = "Hello from PHP! " . (string)phpversion();
 // $parsedJson["test"] = "test";
-$NexssStdout = json_encode($parsedJson, JSON_UNESCAPED_UNICODE);
+if (version_compare(phpversion(), '5.4.0', '<')) {
+    $NexssStdout = json_encode($parsedJson);
+} else {
+    $NexssStdout = json_encode($parsedJson, JSON_UNESCAPED_UNICODE);
+}
+
 
 # STDOUT
 fwrite(STDOUT, $NexssStdout);
