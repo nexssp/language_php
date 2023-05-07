0485329dcc2440d0ebc79065efb6bc16a2afbf6a
Marcin Polak <mapoart@gmail.com>
2023-05-07 13:30:55 +0200

    Support for Android platforms

--- php.android.nexss.config.js
+++ php.android.nexss.config.js
@@ -4,13 +4,13 @@ let sudo = process.sudo;
 
 languageConfig.compilers = {
   php8: {
-    install: `apt install -y php`,
+    install: `pkg install -y php`,
     command: "php",
     args: "<file>",
     help: ``,
   },
   php8ini: {
-    install: `apt install -y php`,
+    install: `pkg install -y php`,
     command: "php",
     args:
       "-c " + require("path").resolve(__dirname + "/etc/php.ini") + " <file>",
@@ -19,16 +19,6 @@ languageConfig.compilers = {
 };
 
 const distro = process.distro;
-
-// This function just replace all apt-get,apt to the right distribution pkg installer.
-languageConfig.compilers.php8.install = process.replacePMByDistro(
-  languageConfig.compilers.php8.install
-);
-
-languageConfig.compilers.php8ini.install = process.replacePMByDistro(
-  languageConfig.compilers.php8ini.install
-);
-
 
 languageConfig.dist = distro;
 
